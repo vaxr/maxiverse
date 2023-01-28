e98f2e8a70500ac2a92cd2bb18678b4f47594bb9
Viktor Ricci <viktor@ricci.codes>
2023-01-28 17:49:07 +0100

    Fix CSF

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+// TODO remove phaser.io
+// TODO deal with unsafe- policies
+// TODO CSF concept
+const contentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' data: blob:;
+    font-src 'self';
+    connect-src 'self' labs.phaser.io;
+`
+
+
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+                    },
+                ],
+            }
+        ]
+    },
 }
 
 module.exports = nextConfig
