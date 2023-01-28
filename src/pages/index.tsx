31ba97817975619e75453f4d93a69edb7db68769
Viktor Ricci <viktor@ricci.codes>
2023-01-28 17:15:17 +0100

    Replace favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Maxiverse</title>
+                <title>Suimax Maxiverse</title>
                 <meta name="description" content="SuiMax Maxiverse Client"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
