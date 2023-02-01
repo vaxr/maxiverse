8fa09735c363fc583a2f746c16b873b9d75f22c8
Viktor Ricci <viktor@ricci.codes>
2023-02-01 09:42:12 +0100

    Add controls and idle/walking animation

--- src/phaser/scenes/demo.tsx
+++ src/phaser/scenes/demo.tsx
@@ -1,8 +1,17 @@
 import {Scene} from 'phaser';
 import {LdtkRoot} from "@/core/ldtk";
 import Sprite = Phaser.GameObjects.Sprite;
+import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
 
 export default class DemoScene extends Scene {
+
+    cursors?: CursorKeys
+    player?: {
+        sprite: Sprite
+        dir: string
+        walking: boolean
+    }
+
     constructor() {
         super('demo');
     }
@@ -38,10 +47,39 @@ export default class DemoScene extends Scene {
         this.addLdtkLayer(map, ldtk, 'bg2')
 
         this.addAnimations()
-        this.addSprite(0, 5, 6)
-        this.addSprite(1, 6, 6)
-        this.addSprite(2, 5, 7)
-        this.addSprite(3, 7, 3)
+        this.addSprite(1, 3, 3).play('1_idle-down')
+        this.addSprite(2, 9, 7).play('2_idle-up')
+        this.addSprite(3, 2, 3).play('3_idle-down')
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+        this.player = {
+            sprite: this.addSprite(0, 5, 6),
+            dir: 'down',
+            walking: false,
+        }
+    }
+
+    update(time: number, delta: number) {
+        super.update(time, delta);
+        if (this.cursors?.down.isDown) {
+            this.updatePlayerAnim(true, 'down')
+        } else if (this.cursors?.up.isDown) {
+            this.updatePlayerAnim(true, 'up')
+        } else if (this.cursors?.right.isDown) {
+            this.updatePlayerAnim(true, 'right')
+        } else if (this.cursors?.left.isDown) {
+            this.updatePlayerAnim(true, 'left')
+        } else if (this.player?.walking) {
+            this.updatePlayerAnim(false)
+        }
+    }
+
+    private updatePlayerAnim(walking: boolean, dir?: string) {
+        if (walking != this.player?.walking || dir && dir != this.player.dir) {
+            this.player!.walking = walking
+            this.player!.dir = dir || this.player!.dir
+            this.player!.sprite.play(`0_${this.player?.walking ? 'walk' : 'idle'}-${this.player?.dir}`)
+        }
     }
 
     private addLdtkLayer(map: Phaser.Tilemaps.Tilemap, ldtk: LdtkRoot, name: string) {
@@ -64,14 +102,22 @@ export default class DemoScene extends Scene {
                     frameRate: 5,
                     repeat: -1,
                 })
+                this.anims.create({
+                    key: `${i}_idle-${dir}`,
+                    frames: this.anims.generateFrameNumbers(DemoScene.SpriteSheets[0], {
+                        frames: [off + 1]
+                    }),
+                    frameRate: 5,
+                    repeat: -1,
+                })
             }
+
         }
     }
 
     private addSprite(i: number, x: number, y: number): Sprite {
-        const sprite = this.add.sprite(16 + 32 * x, 32 * y - 4, DemoScene.SpriteSheets[0]).setScale(0.75, 0.75)
-        sprite.play(`${i}_walk-down`)
+        const sprite = this.add.sprite(16 + 32 * x, 32 * y - 6, DemoScene.SpriteSheets[0]).setScale(0.75, 0.75)
+        sprite.play(`${i}_idle-down`)
         return sprite
     }
 }
-
